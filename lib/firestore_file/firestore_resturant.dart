e0619e44a4d20a38ed06407ee11cda516e8cea69
minchanpark <alscksqkr13@gmail.com>
2024-02-12 13:52:16 +0900

    make a home_page

---
+++ lib/firestore_file/firestore_resturant.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreServiseResturant {
+  final CollectionReference notes =
+      FirebaseFirestore.instance.collection('restaurant');
+
+  //READ: get notes from database
+  Stream<QuerySnapshot> getNotesStream() {
+    final notesStream =
+        notes.orderBy('timestamp', descending: true).snapshots();
+
+    return notesStream;
+  }
+}
